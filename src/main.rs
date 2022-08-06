0d66c185a5e6a38e43efc921b1244f58d84db81a
LinlyBoi <libkyy@e.email>
2022-08-06 23:18:48 +0200

    god help us all

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,18 @@ impl EventHandler for Handler {
             // authentication error, or lack of permissions to post in the
             // channel, so log to stdout when some error happens, with a
             // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await 
+            {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        else if msg.content == "hello" {
+            // Sending a message can fail, due to a network error, an
+            // authentication error, or lack of permissions to post in the
+            // channel, so log to stdout when some error happens, with a
+            // description of it.
+            if let Err(why) = msg.channel_id.say(&ctx.http, "https://www.nohello.com/".await 
+            {
                 println!("Error sending message: {:?}", why);
             }
         }
