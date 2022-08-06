69d4887cdb569fb55201331360cad25ca6219b63
LinlyBoi <libkyy@e.email>
2022-08-06 23:20:53 +0200

    Some bugs fixed

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,8 @@ impl EventHandler for Handler {
     // Event handlers are dispatched through a threadpool, and so multiple
     // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await 
-            {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-        else if msg.content == "hello" {
+
+        if msg.content == "hello" {
             // Sending a message can fail, due to a network error, an
             // authentication error, or lack of permissions to post in the
             // channel, so log to stdout when some error happens, with a
